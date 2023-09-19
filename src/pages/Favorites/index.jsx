a4f49ab9e0e0f2f48c1520bfdffdce8b37115c93
Erick William <erikcwill@gmail.com>
2023-09-19 17:41:35 -0300

    adding favorites function

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,15 +1,21 @@
+import { useFavoriteContext } from "../../contexts/Favorites";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
 import Title from "../../components/Title";
 import styles from "./Favorites.module.css";
 
 export default function Favorites() {
+  const { favorite } = useFavoriteContext();
   return (
     <>
-      <Banner image='favorites'/>
-      <Title><h1>Meus Favoritos!</h1></Title>
+      <Banner image="favorites" />
+      <Title>
+        <h1>Meus Favoritos!</h1>
+      </Title>
       <section className={styles.container}>
-        <Card></Card>
+        {favorite.map((fav) => {
+          return <Card {...fav} key={fav.id} />;
+        })}
       </section>
     </>
   );
